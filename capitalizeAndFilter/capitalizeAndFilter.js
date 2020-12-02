315602ecef016d3aaa8e4e25e39643289be4c934
ryanleviathan <ryan.m.carreras@gmail.com>
2020-12-01 17:42:53 -0800

    capitalizeAndFilter 2nd test pass

--- capitalizeAndFilter/capitalizeAndFilter.js
+++ capitalizeAndFilter/capitalizeAndFilter.js
@@ -1,12 +1,7 @@
 const capitalizeAndFilter = arr => {
-  const newArr = [];
-  for(let i = 0; i < arr.length; i++) {
-    // arr.filter(item => !item.includes('f'))
-    if(!arr[i].includes('f')) {
-      const capitalizedItem = arr[i].toUpperCase();
-      newArr.push(capitalizedItem)
-    } 
-  }
+  const filteredArr = arr.filter(item => !item.includes('f'))
+  newArr = filteredArr.map(item => item.toUpperCase());
+
   return newArr;
 }
 
--- capitalizeAndFilter/capitalizeAndFilter.test.js
+++ capitalizeAndFilter/capitalizeAndFilter.test.js
@@ -4,6 +4,6 @@ const nameOfThings = ['spot', 'rover', 'bingo', 'fred'];
 
 describe('takes an array of strings, capitalizing all strings and filtering out any string that starts with the letter f', () => {
   it('does it', () => {
-    expect(capitalizeAndFilter(nameOfThings)).toEqual(['SPOT', 'ROVER', 'BINGO']);
+    expect(capitalizeAndFilter(nameOfThings)).toEqual(['SPOT', 'ROVER', 'BINGO',]);
   });
 });
